873515f81e835d3fbd03bf99c0d8ae356c2db45c
Igor Spasić <igor@jodd.org>
2014-10-22 17:58:09 +0200

    Added params for sizes

--- src/main/java/com/oblac/jcb/collection/ForLoopOverListBenchmark.java
+++ src/main/java/com/oblac/jcb/collection/ForLoopOverListBenchmark.java
@@ -7,6 +7,7 @@ import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
@@ -15,7 +16,6 @@ import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.infra.Blackhole;
 
 import java.util.ArrayList;
-import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -31,16 +31,25 @@ import java.util.concurrent.TimeUnit;
  # VM invoker: /Library/Java/JavaVirtualMachines/jdk1.8.0_20.jdk/Contents/Home/jre/bin/java
  # VM options: -Dfile.encoding=UTF-8 -Duser.country=US -Duser.language=en -Duser.variant
 
- Benchmark                                                  Mode  Samples    Score   Error   Units
- c.o.j.c.ForLoopOverListBenchmark.forInLoop                thrpt       20  232.383 ± 3.718  ops/ms
- c.o.j.c.ForLoopOverListBenchmark.forIndexLoop             thrpt       20  239.481 ± 1.672  ops/ms
- c.o.j.c.ForLoopOverListBenchmark.forIndexOptimizedLoop    thrpt       20  240.767 ± 3.549  ops/ms * </pre>
-
+ Benchmark                                                 (size)   Mode  Samples    Score   Error   Units
+ c.o.j.c.ForLoopOverListBenchmark.forInLoop                     1  thrpt       20  262.586 ± 3.108  ops/ms
+ c.o.j.c.ForLoopOverListBenchmark.forInLoop                    10  thrpt       20  263.901 ± 2.660  ops/ms
+ c.o.j.c.ForLoopOverListBenchmark.forInLoop                   100  thrpt       20  262.818 ± 4.625  ops/ms
+ c.o.j.c.ForLoopOverListBenchmark.forInLoop                  1000  thrpt       20  238.309 ± 3.818  ops/ms
+ c.o.j.c.ForLoopOverListBenchmark.forInLoop                 10000  thrpt       20  230.548 ± 5.020  ops/ms
+ c.o.j.c.ForLoopOverListBenchmark.forIndexLoop                  1  thrpt       20  277.934 ± 3.958  ops/ms
+ c.o.j.c.ForLoopOverListBenchmark.forIndexLoop                 10  thrpt       20  276.160 ± 3.233  ops/ms
+ c.o.j.c.ForLoopOverListBenchmark.forIndexLoop                100  thrpt       20  276.219 ± 3.745  ops/ms
+ c.o.j.c.ForLoopOverListBenchmark.forIndexLoop               1000  thrpt       20  247.458 ± 4.571  ops/ms
+ c.o.j.c.ForLoopOverListBenchmark.forIndexLoop              10000  thrpt       20  239.914 ± 2.185  ops/ms
+ c.o.j.c.ForLoopOverListBenchmark.forIndexOptimizedLoop         1  thrpt       20  279.704 ± 3.953  ops/ms
+ c.o.j.c.ForLoopOverListBenchmark.forIndexOptimizedLoop        10  thrpt       20  279.319 ± 4.056  ops/ms
+ c.o.j.c.ForLoopOverListBenchmark.forIndexOptimizedLoop       100  thrpt       20  280.749 ± 2.809  ops/ms
+ c.o.j.c.ForLoopOverListBenchmark.forIndexOptimizedLoop      1000  thrpt       20  249.631 ± 1.410  ops/ms
+ c.o.j.c.ForLoopOverListBenchmark.forIndexOptimizedLoop     10000  thrpt       20  243.168 ± 3.077  ops/ms
  * </pre>
  * <p>
- * Conclusion: Enhanced loop if as fast as indexed loop.
- *
- * todo: test on different volumes of data
+ * Conclusion: Enhanced loop if as fast as indexed loop, if not faster.
  */
 @BenchmarkMode(Mode.Throughput)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
@@ -51,11 +60,14 @@ import java.util.concurrent.TimeUnit;
 @State(Scope.Thread)
 public class ForLoopOverListBenchmark {
 
+	@Param({"1", "10", "100", "1000", "10000"})
+	private int size;
+
 	private ArrayList<Integer> arrayList;
 
 	@Setup
 	public void setup() {
-		arrayList = DataGenerator.listOfRandomIntegers(10000);
+		arrayList = DataGenerator.listOfRandomIntegers(size);
 	}
 
 	@Benchmark
